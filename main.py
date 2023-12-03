47e4929d5eba81444c5a886c6b39af442ce55968
BriVandrey <bmvandrey@gmail.com>
2023-12-03 19:04:41 +0000

    day 2 solution

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import d1
+import d1, d2
+
 
 def happy_christmas():
-    print('----------------------------------------------------------------')
-    print('----------------------------------------------------------------')
+    print('                            --                            ')
+    print('                            --                            ')
     print('On the first day of Christmas, we checked our list of strings...')
     d1.solve_d1(path + 'd1.txt')
+    print('                          ------                          ')
+    print('                          ------                          ')
+    print('On the second day of Christmas, we played a silly cube game...')
+    d2.solve_d2(path + 'd2.txt')
 
 
 if __name__ == '__main__':
