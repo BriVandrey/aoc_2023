4574484b7d39977182737a70e19ab4bba191ca83
BriVandrey <bmvandrey@gmail.com>
2023-12-04 10:33:40 +0000

    read in file as numpy array

--- utility.py
+++ utility.py
@@ -1,7 +1,19 @@
-# read in .txt file from specified path
+import numpy as np
+
+
+# read in .txt file from specified path as a list
 def read_file(path):
     with open(path, 'r') as file:
         lines = file.readlines()
         lines = [line.strip() for line in lines]  # remove new line characters
 
     return lines
+
+
+# read in .txt file from specified path as a numpy array
+def read_file_as_array(path):
+    lines = read_file(path)
+    data_array = np.array([list(line) for line in lines], dtype='U1')  # format as 2d array
+
+    return data_array
+
