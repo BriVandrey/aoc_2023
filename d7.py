2dec2e2950e79bdf7bc71ff691d90ab1c11da629
BriVandrey <bmvandrey@gmail.com>
2023-12-10 21:53:56 +0000

    d7 start (q2 not correct)

--- d7.py
+++ d7.py
@@ -19,16 +19,7 @@ def check_for_multiples(values, n_repeats):
     return False
 
 
-def deal_with_jokers(values):
-
-
-
-def categorise_hands(hand, joker=False):
-    values = [char for char in hand]  # split into characters (all str)
-    if joker:
-        values = deal_with_jokers(values)
-    unique = list(set(values))
-
+def categorise_hands_no_jokers(values, unique):
     if len(unique) == 5:
         return 'high_card'
     if len(unique) == 4:
@@ -49,10 +40,46 @@ def categorise_hands(hand, joker=False):
         return 'five_oak'
 
 
-def get_ranks(df):
+def categorise_hands_with_jokers(values, unique):
+    if len(unique) == 5:
+        return 'pair'
+    if len(unique) == 4:
+        return 'three_oak'
+    if len(unique) == 3:
+        result = check_for_multiples(values, 3)
+        if result:
+            return 'four_oak'
+        else:
+            return 'full_house'
+    if len(unique) == 2:
+        result = check_for_multiples(values, 4)
+        if result:
+            return 'five_oak'
+        else:
+            return 'four_oak'
+    if len(unique) == 1:
+        return 'five_oak'
+
+
+def categorise_hands(hand, joker=False):
+    values = [char for char in hand]  # split into characters (all str)
+    unique = list(set(values))
+    if joker and 'J' in unique:
+        result = categorise_hands_with_jokers(values, unique)
+    else:
+        result = categorise_hands_no_jokers(values, unique)
+
+    return result
+
+
+def get_ranks(df, joker=False):
     start_rank = 1  # starting value
     ranked_hands = ['high_card', 'pair', 'two_pair', 'three_oak', 'full_house', 'four_oak', 'five_oak']  # low to high
-    ranked_values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+
+    if joker:  # J is lowest
+        ranked_values = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
+    else:
+        ranked_values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
     for hand in ranked_hands:
         hand_df = df[df['category'] == hand].copy()
@@ -65,9 +92,17 @@ def get_ranks(df):
     return df
 
 
-def get_total_winnings(df):
+def get_total_winnings(data):
+    df = data.copy()
     df['category'] = df['hands'].apply(categorise_hands)
     df = get_ranks(df)
+    df['winnings'] = df['bids'] * df['ranks']
+    return df['winnings'].sum()
+
+
+def get_total_winnings_with_joker(df):
+    df['category'] = df['hands'].apply(categorise_hands, joker=True)
+    df = get_ranks(df, joker=True)
     df['winnings'] = df['bids'] * df['ranks']
     return df['winnings'].sum()
 
@@ -76,7 +111,9 @@ def solve_d7(data_path):
     data = utility.read_file(data_path)
     df = make_df(data)
     q1_answer = get_total_winnings(df)
+    q2_answer = get_total_winnings_with_joker(df)
     print(q1_answer)
+    print(q2_answer)
 
 
 
