979f25807beb3b6b5cf45324873c244f99c705a7
BriVandrey <bmvandrey@gmail.com>
2023-12-17 15:53:27 -0600

    d9 solution

--- d9.py
+++ d9.py
@@ -3,21 +3,25 @@ import re
 import utility
 
 
-def get_last_value(arrays):
+def get_last_value(arrays, idx, subtract=False):
     val_to_return, val = 0, 0  # start with 0's
-    for i in range(0, len(arrays) - 1):
-        next_val = arrays[i + 1][-1]
-        val = next_val + val
-        if i == len(arrays) - 2:  # stop when reaching the second to last value
-            val_to_return = val
+    for i, array in enumerate(arrays[:-1]):  # iterate up to the second to last array
+        next_val = arrays[i + 1][idx]
+        if subtract:
+            val = next_val - val
+        else:
+            val = next_val + val
+        val_to_return = val
+
     return val_to_return
 
 
-def get_history(diffs):
+def get_history(numbers, diffs):
     if np.all(diffs == 0):
         return 0
     else:  # get list of difference arrays
         arrays = []
+        arrays.append(numbers)
         arrays.append(diffs)
 
         while not np.all(diffs == 0):  # check if array is all 0's
@@ -25,25 +29,28 @@ def get_history(diffs):
             arrays.append(diffs)
 
         arrays.reverse()  # reverse so last array (all 0's) is first because who likes going backwards
-        last_value = get_last_value(arrays)
+        last_value = get_last_value(arrays, -1)
+        last_value_front = get_last_value(arrays, 0, subtract=True)
 
-        return last_value
+        return last_value, last_value_front
 
 
 def extrapolate_values(data):
-    vals = []
+    vals, vals_front = [], []
     for line in data:
         numbers = [int(n) for n in re.findall(r'-?\d+', line)]
-        val = get_history(np.diff(numbers))
+        val, val2 = get_history(numbers, np.diff(numbers))
         vals.append(val)
+        vals_front.append(val2)
 
-    return sum(vals)
+    return sum(vals), sum(vals_front)
 
 
 def solve_d9(data_path):
     data = utility.read_file(data_path)
-    q1_answer = extrapolate_values(data)
-    print(q1_answer)
+    q1_answer, q2_answer = extrapolate_values(data)
+    print('The sum of extrapolated values forwards is', q1_answer)
+    print('The sum of extrapolated values backwards is', q2_answer)
 
 
 if __name__ == '__main__':
